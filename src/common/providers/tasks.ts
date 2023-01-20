667913d2aafbdcfa6a26d889e4a0ce16accbe22a
James Whaley <juicy66173@gmail.com>
2023-01-20 16:48:49 +0000

    Patch tasks to include execution info in context

--- src/common/providers/tasks.ts
+++ src/common/providers/tasks.ts
@@ -82,12 +82,54 @@ export interface AuthData {
   token: DecodedIdToken;
 }
 
+/** Metadata about the task invocation. */
+export interface TaskData {
+  /**
+   * The name of the queue.
+   */
+  queue: string;
+
+  /**
+   * The "short" name of the task, or, if no name was specified at creation,
+   * a unique system-generated id.
+   *
+   * This is the `{task}` value in the complete task name, ie.
+   * `projects/{project}/locations/{location}/queues/{queue}/tasks/{task}`.
+   */
+  name: string;
+
+  /**
+   * The number of times this task has been retried. For the first attempt,
+   * this value is `0`. This number includes attempts where the task failed
+   * due to `5XX` error codes and never reached the execution phase.
+   */
+  retryCount: number;
+
+  /**
+   * The total number of times that the task has received a response from the
+   * handler. Since Cloud Tasks deletes the task once a successful response
+   * has been received, all previous handler responses were failures.
+   * This number does not include failures due to 5XX error codes.
+   */
+  executionCount: number;
+
+  /**
+   * The schedule time of the task, specified in seconds since January 1st 1970.
+   */
+  eta: number;
+}
+
 /** Metadata about a call to a Task Queue function. */
 export interface TaskContext {
   /**
    * The result of decoding and verifying an ODIC token.
    */
   auth?: AuthData;
+
+  /**
+   * Task-specific information.
+   */
+  task?: TaskData;
 }
 
 /**
@@ -103,6 +145,11 @@ export interface Request<T = any> {
    * The result of decoding and verifying an ODIC token.
    */
   auth?: AuthData;
+
+  /**
+   * Task-specific information.
+   */
+  task?: TaskData;
 }
 
 type v1TaskHandler = (data: any, context: TaskContext) => void | Promise<void>;
@@ -132,6 +179,39 @@ export function onDispatchHandler<Req = any>(
         context.auth = {
           uid: authToken.uid,
           token: authToken,
+        };
+
+        const invalidHeaders: [string, string | null][] = [];
+        const queueName = req.header("X-CloudTasks-QueueName");
+        if (queueName == null) {
+          invalidHeaders.push(["X-CloudTasks-QueueName", null]);
+        }
+        const taskName = req.header("X-CloudTasks-TaskName");
+        if (taskName == null) {
+          invalidHeaders.push(["X-CloudTasks-TaskName", null]);
+        }
+        const retryCount = req.header("X-CloudTasks-TaskRetryCount");
+        if (retryCount == null || !/^\d+$/.test(retryCount)) {
+          invalidHeaders.push(["X-CloudTasks-TaskRetryCount", null]);
+        }
+        const executionCount = req.header("X-CloudTasks-TaskExecutionCount");
+        if (executionCount == null || !/^\d+$/.test(executionCount)) {
+          invalidHeaders.push(["X-CloudTasks-TaskExecutionCount", executionCount ?? null]);
+        }
+        const taskEta = req.header("X-CloudTasks-TaskETA");
+        if (taskEta == null || !/^\d+$/.test(taskEta)) {
+          invalidHeaders.push(["X-CloudTasks-TaskETA", taskEta ?? null]);
+        }
+        if (invalidHeaders.length > 0) {
+          logger.error("Invalid cloud task headers", { headers: invalidHeaders });
+          throw new https.HttpsError("internal", "Invalid cloud task headers", invalidHeaders);
+        }
+        context.task = {
+          queue: queueName,
+          name: taskName,
+          retryCount: Number(retryCount),
+          executionCount: Number(executionCount),
+          eta: Number(taskEta),
         };
       }
 
